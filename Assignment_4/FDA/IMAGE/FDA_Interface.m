052fe7ee61a698ea44ef6515a6f6dfdb8652566f
Neha Muthiyan <muthiyanneha4@gmail.com>
2016-11-18 21:03:51 +0530

    Plots for real FDA

--- Assignment_4/FDA/IMAGE/FDA_Interface.m
+++ Assignment_4/FDA/IMAGE/FDA_Interface.m
@@ -1,7 +1,7 @@
 function [] = FDA_Interface()
 
 	%%%%%%%%%%%%% enter path as argument %%%%%%%%%%%%%%%%%%
-	clusters = 1;
+	clusters = 6;
 	[TrainData , TestData] = datasets_image('coast/','mountain/','insidecity/');  %Pass the relative path to the data sets(M .txt files denoting M classes), with a trailing '/'    
 	% size(TrainData{1})
 	%%%%%%%%%%%% 1D data with maximum discrimination %%%%%%%%%%
